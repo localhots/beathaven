9bc526cbb5cab9b8021a4e8fcce94eb91102c850
Gregory Eremin <magnolia_fan@me.com>
2012-08-26 00:48:13 +0400

    Bootstrapped rails

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source :rubygems
+
+gem "rails", "3.2.8"
+gem "pg"
+
+group :assets do
+  gem "sass-rails",   "~> 3.2.3"
+  gem "coffee-rails", "~> 3.2.1"
+
+  gem "therubyracer", platforms: :ruby
+
+  gem "uglifier", ">= 1.0.3"
+end
+
+gem "jquery-rails"
+
+# To use Jbuilder templates for JSON
+# gem "jbuilder"
+
+gem "thin"
+
+# Deploy with Capistrano
+gem "capistrano"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeatHaven::Application.config.secret_token = '7ddc72b05ebc16550357e948d923dae427df5f66b883b112bf87fbc912bfe0db993779109173dffde47173691ed1f4904a114138b77a9369f5bc5b720dfe4a03'
